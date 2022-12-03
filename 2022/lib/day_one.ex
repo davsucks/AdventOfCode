e8987f8550d611bfa11f9d16ee9e31298f858b85
David Suckstorff <dsucksto@thoughtworks.com>
2022-12-02 18:36:38 -0600

    Day one - 2022

---
+++ 2022/lib/day_one.ex
@@ -0,0 +1,47 @@
+defmodule DayOne do
+  def partOne do
+    input =
+      File.read!("input/day_one.txt")
+      |> String.split("\n")
+      |> Enum.chunk_while(
+        [],
+        fn element, acc ->
+          if "" == String.trim(element) do
+            {:cont, Enum.sum(acc), []}
+          else
+            {:cont, [String.to_integer(element) | acc]}
+          end
+        end,
+        fn
+          [] -> {:cont, []}
+          acc -> {:cont, Enum.sum(acc)}
+        end
+      )
+      |> Enum.sort(:desc)
+      [head | _tail] = input
+      head
+  end
+
+  def partTwo do
+    input =
+      File.read!("input/day_one.txt")
+      |> String.split("\n")
+      |> Enum.chunk_while(
+        [],
+        fn element, acc ->
+          if "" == String.trim(element) do
+            {:cont, Enum.sum(acc), []}
+          else
+            {:cont, [String.to_integer(element) | acc]}
+          end
+        end,
+        fn
+          [] -> {:cont, []}
+          acc -> {:cont, Enum.sum(acc)}
+        end
+      )
+      |> Enum.sort(:desc)
+      |> Enum.slice(0, 3)
+      |> Enum.sum()
+  end
+end
